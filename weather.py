bf9ec29056cb22d9ec442f6dfd9d0f9819fb447a
Josh Gordon <github@joshgordon.net>
2013-07-26 03:21:15 +0000

    Fixed some spellings, conditions are no longer duplicated.

--- weather.py
+++ weather.py
@@ -11,7 +11,8 @@ print """\
 <head>  <meta charset="utf-8">
   <title>Most Precise Weather</title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
-<body class="python">"""
+<body class="python">
+<h1>Weather Conditions. Very Precise<a href="https://en.wikipedia.org/wiki/Accuracy_and_precision">*</a></h1>"""
 temp = str(random.randint(15, 35) + random.random()) + str(random.randint(100000000000000000000000000000000000000,100000000000000000000000000000000000000000))
 tempF = str(9.0/5.0 * float(temp) + 32) + str(random.randint(100000000000000000000000000000000000000,100000000000000000000000000000000000000000))
 
@@ -20,11 +21,14 @@ windmph = str(float(wind) * 2.23694) + str(random.randint(1000000000000000000000
 
 rh = str(random.randint(0, 99) + random.random()) + str(random.randint(10000000000000000000000000000000000000,100000000000000000000000000000000000000000))
 
-values = {"clear", "rain", "wind", "hail", "sleet", "showers", "hurricane", "tornado", "apocolypse"} 
+values = {"clear", "rain", "wind", "hail", "sleet", "showers", "hurricane", "tornado", "apocalypse"} 
 
-condition = str(random.sample(values, 1)[0]) + " with a chance of " + str(random.sample(values, 1)[0]) + "." 
+conditionArray = random.sample(values, 2)
+condition = conditionArray[0] + " with a chance of " + conditionArray[1] + "." 
 
 direction = {"N", "S", "W", "E", "NE", "NW", "SE", "SW", "UP", "DOWN", "SLANTWAYS", "IN YOUR FACE"}
+
+
 
 print "Current Temperature: " + temp + " ºC (" + str(tempF) + " ºF)" + "<br>"
 print "Current Wind velocity: " + wind + " m/s (" + str(windmph) + " mph) " + str(random.sample(direction, 1)[0]) + "<br>"
@@ -36,7 +40,9 @@ print condition
 print """\
 <br><br>
 Inspired by the less precise <a href="http://preciseweather.net">Precise Weather</a><br>
-Fork my <del>hideously ugly</del> code that was hacked together in 20 minutes on <a href="http://github.com/joshgordon/precise-weather/">Github</a>
+Fork my <del>hideously ugly</del> code that was hacked together in 20 minutes on <a href="http://github.com/joshgordon/precise-weather/">Github</a><br>
+<a href="http://joshgordon.net">Main Site</a>
+
 </body>
 </html>
 """
